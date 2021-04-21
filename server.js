d4223b6d5f0ed41551ffa72a611875aaa9565b93
mar1n <30738216+mar1n@users.noreply.github.com>
2021-04-21 13:03:58 +0100

    customer routes

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+require("./app/routes/customer.routes");
+
 // set port, listen for requests
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
